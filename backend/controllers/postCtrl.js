5df71fea1a4310f3ae1bed0470c2e3fd5cfbecea
Laure-Anais Giowachini <giowachini.la@gmail.com>
2021-12-17 11:45:50 +0100

    Work on like & dislike

--- backend/controllers/postCtrl.js
+++ backend/controllers/postCtrl.js
@@ -2,6 +2,7 @@ const db = require("../models");
 const fs = require('fs');
 
 const schemaPost = require("../schema/postSchema");
+const { likes } = require("../models");
 
 exports.createPost = async (req, res, next) => {
   try {
@@ -131,7 +132,59 @@ exports.getAllPosts = (req, res, next) => {
     .catch((error) => res.status(400).json({ error }));
 };
 
-// 
+exports.likeDislikePosts = async (req, res, next) => {
+  try {
+     // 1- Vérification des données reçues du front
+     // likes
+      if (req.body.postlikes){
+          db.likes.postlikes = 1,
+          db.likes.postdislikes = 0
+      } 
+      // dislikes
+      if (req.body.postdislikes) {
+        db.likes.postdislikes = 1,
+        db.likes.postlikes = 0
+      }
+      const tokenDB = await db.users.findOne(
+        {where:
+          {id: req.user.userId}
+        }
+      ) 
+      if (!tokenDB) {res.status(401).json({ message : "Merci de vous identifier" })
+
+      // 2- Traitement des données (préparation de requete)
+      const userlikeDB = db.likes.findOne(
+        {where:
+        {id_users: tokenDB.id,
+        id_posts:req.params.id_posts}
+        }
+      )
+      const postDB = db.posts.findOne(
+        {where:
+          {id: req.params.id}
+        }
+      )
+      if(!postDB) {res.status(401).json({ message : "Post non trouvé" })}
+      
+    // 3- Echange avec la base de données
+      if(userlikeDB.postlikes ||  userlikeDB.postdislikes) {
+        userlikeDB.update ({ ...likes})
+        } {
+          res.status(200).json({ message : "Votre choix a été pris en compte" })
+        }
+         
+      if(!userlikeDB.postlikes && !userlikeDB.postdislikes) {
+        likes.id_users = tokenDB.id,
+        likes.id_posts = postDB.id
+        userlikeDB.create ( likes )
+        }
+
+    }
+  } catch(err) {
+    return res.status(500).json({err});
+ } 
+  
+};
 
 // 1. recherche si dans ta table utilisateur a déjà liké ou disliké ce post
 //  - Si OUI
